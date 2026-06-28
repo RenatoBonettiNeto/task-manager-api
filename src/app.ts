8871ade7a9eabb1afc234461ebcd28bb77d687b5
RenatoBonettiNeto <bonettineto.renato03@gmail.com>
2026-06-28 19:02:16 -0300

    Configurada validação para adicionar o sessionID no usuário ao cria-lo.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import fastify from "fastify"
+import cookie from "@fastify/cookie";
 import { userRoutes } from "./routes/users.js";
 
 export const app = fastify()
+
+app.register(cookie);
 
 app.register(userRoutes, {
   prefix: "/users",
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,7 @@ import type { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { db } from "../database.js";
 import crypto, { randomUUID } from "node:crypto";
-import { request } from "node:http";
+import { checkSessionIdExists } from "../middleware/check-session-id.js";
 
 export function userRoutes(app: FastifyInstance) {
   app.addHook("preHandler", async (request, reply) => {
@@ -16,8 +16,20 @@ export function userRoutes(app: FastifyInstance) {
 
     const { name } = createUsersBodySchema.parse(request.body);
 
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
     await db("user").insert({
       id: crypto.randomUUID(),
+      session_id: sessionId,
       name,
     });
 
